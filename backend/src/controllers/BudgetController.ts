8fe75caf77401a229d3ea42c2509828ade8c7553
Angel Chacon <angel1998chacon@gmail.com>
2025-02-25 21:47:00 -0600

    Controlador ID Nuevo

--- backend/src/controllers/BudgetController.ts
+++ backend/src/controllers/BudgetController.ts
@@ -4,7 +4,14 @@ export class BudgetController {
     static getAll = async (req : Request, res: Response) => {
         console.log('hola mmwebo')
     }
+
+
     static create = async (req : Request, res: Response) => {
+        console.log('hola mmwebo desde post')
+    }
+
+
+    static getById = async (req : Request, res: Response) => {
         console.log('hola mmwebo desde post')
     }
  
